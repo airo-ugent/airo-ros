9593003f00d80a6365089b3e9ee517866d7b7761
Thomas Lips <37955681+tlpss@users.noreply.github.com>
2022-01-25 15:58:59 +0100

    Moveit mgi bridge (#9)
    Bridge over ROS to address the Move Group Interface from any other ROS client library (in particular Python) or the CLI.
    
    * change to source docker file
    * use moveit source underlay.
    * launchfile for MGI bridge
    * change adapter point to make frames consistent between EEF and TCP

--- ROS/src/moveit/airo_moveit_mgi_bridge/launch/mgi_bridge.launch.py
+++ ROS/src/moveit/airo_moveit_mgi_bridge/launch/mgi_bridge.launch.py
@@ -4,8 +4,9 @@ import yaml
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def load_file(package_name, file_path):
@@ -31,38 +32,198 @@ def load_yaml(package_name, file_path):
 
 
 def generate_launch_description():
-    # planning_context
     declared_arguments = []
 
+    # planning_context
     declared_arguments.append(
         DeclareLaunchArgument(
             "planning_group",
-            default_value="panda_arm",
+            default_value="ur_robotiq",
             description="Which group to plan for. Must be defined in the SRDF.",
         )
     )
+    # UR specific arguments
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "ur_type",
+            description="Type/series of used UR robot.",
+            choices=["ur3", "ur3e", "ur5", "ur5e", "ur10", "ur10e", "ur16e"],
+        )
+    )
 
-    # load params
+    declared_arguments.append(
+        DeclareLaunchArgument("robot_ip", description="IP address by which the robot can be reached.")
+    )
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "prefix",
+            default_value='""',
+            description="Prefix of the joint names, useful for \
+        multi-robot setup. If changed than also joint names in the controllers' configuration \
+        have to be updated.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_fake_hardware",
+            default_value="false",
+            description="Start robot with fake hardware mirroring command to its states.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "fake_sensor_commands",
+            default_value="false",
+            description="Enable fake command interfaces for sensors used for simple simulations. \
+            Used only if 'use_fake_hardware' parameter is true.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "initial_joint_controller",
+            default_value="joint_trajectory_controller",
+            description="Initially loaded robot controller.",
+        )
+    )
+
+    # Initialize Arguments
+    ur_type = LaunchConfiguration("ur_type")
+    robot_ip = LaunchConfiguration("robot_ip")
+    prefix = LaunchConfiguration("prefix")
+    use_fake_hardware = LaunchConfiguration("use_fake_hardware")
+    fake_sensor_commands = LaunchConfiguration("fake_sensor_commands")
+
+    # other variables.
+    description_package = "ur_robotiq_85_description"
+    description_file = "ur_robotiq_85.urdf.xacro"
+
+    ur_description_package = "ur_description"
+    calibration_package = "ur_description"  # link to package containing the robot calibration (kinematics file).
+
+    moveit_config_package = "ur_robotiq_85_moveit_config"
+    moveit_config_file = "ur_robotiq_85.srdf.xacro"
+
+    robot_name = "ur"  # can be changed but changes must be matched in the controllers.yaml file for moveit!
+
     planning_group_param = LaunchConfiguration("planning_group")
 
-    robot_description_config = load_file("moveit_resources_panda_description", "urdf/panda.urdf")
-    robot_description = {"robot_description": robot_description_config}
+    # generate the URDF of the robot + gripper.
+    joint_limit_params = PathJoinSubstitution(
+        [FindPackageShare(ur_description_package), "config", ur_type, "joint_limits.yaml"]
+    )
+    kinematics_params = PathJoinSubstitution(
+        [FindPackageShare(calibration_package), "config", ur_type, "default_kinematics.yaml"]
+    )
+    physical_params = PathJoinSubstitution(
+        [FindPackageShare(ur_description_package), "config", ur_type, "physical_parameters.yaml"]
+    )
+    visual_params = PathJoinSubstitution(
+        [FindPackageShare(ur_description_package), "config", ur_type, "visual_parameters.yaml"]
+    )
+    script_filename = PathJoinSubstitution([FindPackageShare("ur_robot_driver"), "resources", "ros_control.urscript"])
+    input_recipe_filename = PathJoinSubstitution(
+        [FindPackageShare("ur_robot_driver"), "resources", "rtde_input_recipe.txt"]
+    )
+    output_recipe_filename = PathJoinSubstitution(
+        [FindPackageShare("ur_robot_driver"), "resources", "rtde_output_recipe.txt"]
+    )
 
-    robot_description_semantic_config = load_file("moveit_resources_panda_moveit_config", "config/panda.srdf")
-    robot_description_semantic = {"robot_description_semantic": robot_description_semantic_config}
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution([FindPackageShare(description_package), "urdf", description_file]),
+            " ",
+            "robot_ip:=",
+            robot_ip,
+            " ",
+            "joint_limit_params:=",
+            joint_limit_params,
+            " ",
+            "kinematics_params:=",
+            kinematics_params,
+            " ",
+            "physical_params:=",
+            physical_params,
+            " ",
+            "visual_params:=",
+            visual_params,
+            " ",
+            "safety_limits:=",
+            "true",
+            " ",
+            "safety_pos_margin:=",
+            "0.15",
+            " ",
+            "safety_k_position:=",
+            "20",
+            " ",
+            "name:=",
+            # ur_type parameter could be used but then the planning group names in yaml
+            # configs has to be updated!
+            robot_name,
+            " ",
+            "script_filename:=",
+            script_filename,
+            " ",
+            "input_recipe_filename:=",
+            input_recipe_filename,
+            " ",
+            "output_recipe_filename:=",
+            output_recipe_filename,
+            " ",
+            "prefix:=",
+            prefix,
+            " ",
+            "use_fake_hardware:=",
+            use_fake_hardware,
+            " ",
+            "fake_sensor_commands:=",
+            fake_sensor_commands,
+            " ",
+            "headless_mode:=",
+            "false",
+            " ",
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
 
-    kinematics_yaml = load_yaml("moveit_resources_panda_moveit_config", "config/kinematics.yaml")
+    # END OF UR DESCRIPTION
+
+    # MOVEIT
+
+    # MoveIt Configuration
+    robot_description_semantic_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution([FindPackageShare(moveit_config_package), "srdf", moveit_config_file]),
+            " ",
+            "name:=",
+            # Also ur_type parameter could be used but then the planning group names in yaml
+            # configs has to be updated!
+            robot_name,
+            " ",
+            "prefix:=",
+            prefix,
+            " ",
+        ]
+    )
+    robot_description_semantic = {"robot_description_semantic": robot_description_semantic_content}
+
+    kinematics_yaml = load_yaml(moveit_config_package, "config/kinematics.yaml")
+    robot_description_kinematics = {"robot_description_kinematics": kinematics_yaml}
 
     planning_group = {"planning_group": planning_group_param}
 
-    # MoveGroupInterface demo executable
     mgi_bridge = Node(
         name="moveit_mgi_bridge",
         package="airo_moveit_mgi_bridge",
         executable="moveit_mgi_bridge",
         # namespace='moveit_mgi_bridge', # defaulted to node name.
         output="screen",
-        parameters=[robot_description, robot_description_semantic, kinematics_yaml, planning_group],
+        parameters=[robot_description, robot_description_semantic, robot_description_kinematics, planning_group],
     )
 
     return LaunchDescription(declared_arguments + [mgi_bridge])
